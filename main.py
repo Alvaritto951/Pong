32e0513755192fe5a6c24834405f5aab70797633
Alvaritto951 <alvaritto951@gmail.com>
2022-08-29 23:27:21 +0200

    g:/BOOTCAMP - APRENDE A PROGRAMAR FULLSTACK JR/PYTHON/KATAS/Pong/venv/Scripts/python.exe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import random
+
 from entidades import Bola, Raqueta
 
 pg.init()
@@ -12,6 +12,7 @@ bola = Bola(400, 300)
 raqueta1 = Raqueta(20, 300, w=20, h=120, color=(0, 255, 0))
 #Raqueta2 = Pos en x - ancho entre 2 = 
 raqueta2 = Raqueta(780, 300, w=20, h=120, color=(0, 255, 0))
+raqueta2.vy = 5
 
 game_over = False
 while not game_over:
@@ -19,6 +20,18 @@ while not game_over:
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
+        elif evento.type == pg.KEYDOWN:
+            if evento.key == pg.K_DOWN:
+                raqueta2.center_y += raqueta2.vy
+            elif evento.key == pg.K_UP:
+                raqueta2.center_y -= raqueta2.vy
+    
+    estado_teclas = pg.key.get_pressed()
+    if estado_teclas[pg.K_UP]:
+        raqueta2.center_y -= raqueta2.vy
+    if estado_teclas[pg.K_DOWN]:
+        raqueta2.center_y += raqueta2.vy
+ 
     pantalla_principal.fill((0, 0, 0))
     bola.dibujar(pantalla_principal)
     raqueta1.dibujar(pantalla_principal)
