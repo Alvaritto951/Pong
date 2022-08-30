b405ae78b4d60de89bf85a4dd4f439cf4a657c2e
Alvaritto951 <alvaritto951@gmail.com>
2022-08-30 22:04:22 +0200

    Raquetas móviles y limitadas

--- entidades.py
+++ entidades.py
@@ -34,3 +34,18 @@ class Raqueta:
     
     def dibujar(self, pantalla):
         pg.draw.rect(pantalla, self.color, (self.center_x - self.w // 2, self.center_y - self.h // 2, self.w, self.h))
+    
+    def mover(self, tecla_arriba, tecla_abajo, y_max=600): #Son parámetros que luego se indican en main con la tecla que quieres pulsar y ver el máximo de pantalla (ymax)
+        estado_teclas = pg.key.get_pressed() #Devuelve lista de 512 teclas -> Función que indica estado del teclado -> De cada tecla devuelve False si no tocas nada
+        if estado_teclas[tecla_arriba]: #Tecla que se va a pulsar hacia arriba
+            self.center_y -= self.vy
+        if self.center_y < 0 + self.h // 2: #Movimiento de la raqueta hacia arriba
+            self.center_y = self.h // 2
+        if estado_teclas[tecla_abajo]:
+            self.center_y += self.vy
+        if self.center_y > y_max - self.h // 2: #Movimiento de la raqueta hacia abajo
+            self.center_y = y_max - self.h // 2
+
+
+
+    
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pygame as pg
 
 from entidades import Bola, Raqueta
 
-pg.init()
+pg.init() #Se utiliza para iniciar pygame
 
 pantalla_principal = pg.display.set_mode((800, 600))
 pg.display.set_caption("Pong Álvaro")
+cronometro = pg.time.Clock() #Se utiliza para fijar los milisegundos y convertir en FPS librería/módulo/función-clase
 
 bola = Bola(400, 300)
 #Raqueta1 = Pos en y - altura entre 2 = 300 - 60 = 240
@@ -13,24 +14,21 @@ raqueta1 = Raqueta(20, 300, w=20, h=120, color=(0, 255, 0))
 #Raqueta2 = Pos en x - ancho entre 2 = 
 raqueta2 = Raqueta(780, 300, w=20, h=120, color=(0, 255, 0))
 raqueta2.vy = 5
+raqueta1.vy = 5
 
+#Bucle más importante para pygame 
 game_over = False
 while not game_over:
+    dt = cronometro.tick(60) #dt = diferencial de tiempo -> Indicar los FPS dentro de los () -> 60 FPS /// Devuelve los milisegundos que tarda en mostrar en pantalla
+    print(dt)
     #Eventos que hace el usuario y devuelve una lista de eventos
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
-        elif evento.type == pg.KEYDOWN:
-            if evento.key == pg.K_DOWN:
-                raqueta2.center_y += raqueta2.vy
-            elif evento.key == pg.K_UP:
-                raqueta2.center_y -= raqueta2.vy
+
     
-    estado_teclas = pg.key.get_pressed()
-    if estado_teclas[pg.K_UP]:
-        raqueta2.center_y -= raqueta2.vy
-    if estado_teclas[pg.K_DOWN]:
-        raqueta2.center_y += raqueta2.vy
+    raqueta2.mover(pg.K_UP, pg.K_DOWN) #Aquí estaba lo que está indicado en la función "mover" en entidades
+    raqueta1.mover(pg.K_w, pg.K_s)
  
     pantalla_principal.fill((0, 0, 0))
     bola.dibujar(pantalla_principal)
@@ -38,3 +36,4 @@ while not game_over:
     raqueta2.dibujar(pantalla_principal)
     #Le pasa la información a la tarjeta gráfica
     pg.display.flip()
+#Hasta aquí
